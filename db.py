3d2f4664ad1d3d7365bae2473b80707408d8c229
SergeyKalutsky <skalutsky@gmail.com>
2023-07-04 17:03:18 +0300

    first commit

---
+++ db.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+def insert_player(player_id, username):
+    con = sqlite3.connect('db.db')
+    cur = con.cursor()
+    sql = f"INSERT INTO players (player_id, username) VALUES ('{player_id}', '{username}')"
+    cur.execute(sql)
+    con.commit()
+    con.close()
+
+
+def players_amount():
+    con = sqlite3.connect('db.db')
+    cur = con.cursor()
+    sql = 'SELECT * FROM players'
+    cur.execute(sql)
+    rows = cur.fetchall()
+    return len(rows)
+
+
+def get_mafia_usernames():
+    con = sqlite3.connect('db.db')
+    cur = con.cursor()
+    sql = f"SELECT username FROM players WHERE role = 'mafia' "
+    cur.execute(sql)
+    data = cur.fetchall()
+    names = ''
+    for row in data:
+        name = row[0]
+        names += name + '\n'
+    con.close()
+    return names
